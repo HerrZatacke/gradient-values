e0ad32d567af4aa8805a293a6b2af6eb5aa553bc
Andreas Hahn <zatacke@gmail.com>
2023-07-14 20:23:27 +0200

    prepare pages branch

--- README.md
+++
@@ -1,3 +0,0 @@
-# gradient-values
-
-Simple tool to generate gradient values for use in the [dashboy camera](https://github.com/Raphael-Boichot/Mitsubishi-M64282FP-dashcam)
--- scripts/setupServer.js
+++
@@ -1,7 +0,0 @@
-const setup = (app) => {
-  app.use('/api', (req, res) => {
-    res.json({ message: 'hi from an example express middleware' });
-  });
-};
-
-export default setup;
